da6aae38a4e848608acdde7616f0dfaa1741e805
handoyoferi <ferihandoyo1304@gmail.com>
2024-08-08 00:08:47 +0700

    add and config sentry and finished section my experience

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,23 @@
-import Image from "next/image";
-import Hero from "./components/Hero";
-import { FloatingNav } from "./components/ui/FloatingNav";
-import { FaHome } from "react-icons/fa";
+import { navItems } from "@/data";
 import Grid from "./components/Grid";
+import Hero from "./components/Hero";
 import RecentProjects from "./components/RecentProjects";
+import { FloatingNav } from "./components/ui/FloatingNav";
+import Client from "./components/Client";
+import Experience from "./components/Experience";
+import Approach from "./components/Approach";
 
 export default function Home() {
   return (
-    <main className="relative bg-black  flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+    <main className="relative bg-black-200  flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <FloatingNav
-          navItems={[{ name: "Home", link: "/", icon: <FaHome /> }]}
-        />
+        <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
         <RecentProjects />
+        <Client />
+        <Experience />
+        <Approach />
       </div>
     </main>
   );
