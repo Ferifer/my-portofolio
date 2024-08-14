a14a95bba16dc9bf0a3652fe2b3f281d73bd0b98
ferifer <ferihandoyo04@gmail.com>
2024-08-14 20:42:54 +0700

    fixing

--- app/page.tsx
+++ app/page.tsx
@@ -10,15 +10,6 @@ import Experience from "./components/Experience";
 import Approach from "./components/Approach";
 import Footer from "./components/Footer";
 
-// import Hero from "@/components/Hero";
-// import Grid from "@/components/Grid";
-// import Footer from "@/components/Footer";
-// import Clients from "@/components/Clients";
-// import Approach from "@/components/Approach";
-// import Experience from "@/components/Experience";
-// import RecentProjects from "@/components/RecentProjects";
-// import { FloatingNav } from "@/components/ui/FloatingNavbar";
-
 const Home = () => {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
--- data/index.ts
+++ data/index.ts
@@ -76,62 +76,38 @@ export const projects = [
     des: "Explore the wonders of our solar system with this captivating 3D simulation of the planets using Three.js.",
     img: "/p1.svg",
     iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/fm.svg"],
-    link: "https://github.com/adrianhajdin?tab=repositories",
-  },
-  {
-    id: 2,
-    title: "Yoom - Video Conferencing App",
-    des: "Simplify your video conferencing experience with Yoom. Seamlessly connect with colleagues and friends.",
-    img: "/p2.svg",
-    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/stream.svg", "/c.svg"],
-    link: "https://github.com/adrianhajdin/zoom-clone",
-  },
-  {
-    id: 3,
-    title: "AI Image SaaS - Canva Application",
-    des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
-    img: "/p3.svg",
-    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
-    link: "https://github.com/adrianhajdin/ai_saas_app",
-  },
-  {
-    id: 4,
-    title: "Animated Apple Iphone 3D Website",
-    des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
-    img: "/p4.svg",
-    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
-    link: "https://github.com/adrianhajdin/iphone",
+    link: "/ui.earth.com",
   },
 ];
 
 export const testimonials = [
   {
     quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      "Collaborating with Feri was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Feri's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Feri is the ideal partner.",
     name: "Michael Johnson",
     title: "Director of AlphaStream Technologies",
   },
   {
     quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      "Collaborating with Feri was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Feri's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Feri is the ideal partner.",
     name: "Michael Johnson",
     title: "Director of AlphaStream Technologies",
   },
   {
     quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      "Collaborating with Feri was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Feri's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Feri is the ideal partner.",
     name: "Michael Johnson",
     title: "Director of AlphaStream Technologies",
   },
   {
     quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      "Collaborating with Feri was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Feri's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Feri is the ideal partner.",
     name: "Michael Johnson",
     title: "Director of AlphaStream Technologies",
   },
   {
     quote:
-      "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
+      "Collaborating with Feri was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Feri's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Feri is the ideal partner.",
     name: "Michael Johnson",
     title: "Director of AlphaStream Technologies",
   },
@@ -178,40 +154,17 @@ export const workExperience = [
     className: "md:col-span-2",
     thumbnail: "/exp1.svg",
   },
-  {
-    id: 2,
-    title: "Mobile App Dev - JSM Tech",
-    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
-    className: "md:col-span-2", // change to md:col-span-2
-    thumbnail: "/exp2.svg",
-  },
-  {
-    id: 3,
-    title: "Freelance App Dev Project",
-    desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
-    className: "md:col-span-2", // change to md:col-span-2
-    thumbnail: "/exp3.svg",
-  },
-  {
-    id: 4,
-    title: "Lead Frontend Developer",
-    desc: "Developed and maintained user-facing features using modern frontend technologies.",
-    className: "md:col-span-2",
-    thumbnail: "/exp4.svg",
-  },
 ];
 
 export const socialMedia = [
   {
     id: 1,
+    url: "https://github.com/Ferifer?tab=repositories",
     img: "/git.svg",
   },
   {
     id: 2,
-    img: "/twit.svg",
-  },
-  {
-    id: 3,
+    url: "https://www.linkedin.com/in/feri-handoyo-a5a657128/",
     img: "/link.svg",
   },
 ];
